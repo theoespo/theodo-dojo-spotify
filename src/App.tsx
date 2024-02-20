4e07b23ae0ae9aa93102df6ed7dd89659a4374c8
theoespo <espotheo@gmail.com>
2024-02-20 19:06:11 +0100

    nouveau texte

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
         <h1 className="App-title">Bienvenue sur le blind test</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
+        <p>Nouveau texte</p>
       </div>
       <div className="App-buttons"></div>
     </div>
